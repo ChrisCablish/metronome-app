1c3d5a26693b6d9a1787d3a54b080eaa317b5074
Chris <chriscablish@hotmail.com>
2023-05-05 12:43:58 -0500

    replace setInterval() with setTimeout() for more precise timing

--- script.js
+++ script.js
@@ -1,56 +1,108 @@
+// const tempoRangeInput = document.getElementById('tempo-range');
+// const tempoDisplay = document.getElementById('tempo-display');
+// const clickSound = document.getElementById('click-sound');
+// const start = document.getElementById('start-button');
+// const stop = document.getElementById('stop-button');
+
+// clickSound.volume = 1;
+
+// let togglePlay = false;
+
+
+// //GET DEFAULT MET VALUE & DISPLAY IT = 130 (min + (max - min))
+// let currentTempo = tempoRangeInput.value;
+// tempoDisplay.textContent = `${currentTempo} BPM`
+// let millisecondsBetweenClicks = convertBpmToMilliseconds(currentTempo);
+
+// // Function to convert bpm to milliseconds for setInterval() function
+// function convertBpmToMilliseconds(bpm) {
+//     const beatsPerSecond = bpm / 60;
+//     const msPerBeat = 1000 / beatsPerSecond;
+//     return msPerBeat;
+//   }
+
+// // Add an event listener to the tempo range input
+// tempoRangeInput.addEventListener('input', function() {
+//   currentTempo = tempoRangeInput.value;
+//   tempoDisplay.textContent = `${currentTempo} BPM`;
+//   millisecondsBetweenClicks = convertBpmToMilliseconds(currentTempo);
+
+//   //THIS SECTION ALLOWS TEMPO TO BE ADJUSTED DURING PLAYBACK
+//   if (togglePlay === true) {
+//     clearInterval(intervalID);
+//     intervalID = setInterval(() => {
+//       clickSound.currentTime = 0;
+//       clickSound.play();
+//   }, millisecondsBetweenClicks);
+//   } else if (togglePlay === false) {
+//     return
+//   }
+// });
+
+// let intervalID
+
+// start.addEventListener('click', () => {
+//     togglePlay = true;
+//     intervalID = setInterval(() => {
+//         clickSound.currentTime = 0;
+//         clickSound.play();
+//     }, millisecondsBetweenClicks);
+// });
+
+// stop.addEventListener('click', () => {
+//     togglePlay = false;
+//     clearInterval(intervalID);
+// });
+
+
 const tempoRangeInput = document.getElementById('tempo-range');
 const tempoDisplay = document.getElementById('tempo-display');
 const clickSound = document.getElementById('click-sound');
 const start = document.getElementById('start-button');
 const stop = document.getElementById('stop-button');
 
-
 let togglePlay = false;
-
+let timeoutID;
 
 //GET DEFAULT MET VALUE & DISPLAY IT = 130 (min + (max - min))
 let currentTempo = tempoRangeInput.value;
 tempoDisplay.textContent = `${currentTempo} BPM`
 let millisecondsBetweenClicks = convertBpmToMilliseconds(currentTempo);
 
-// Function to convert bpm to milliseconds for setInterval() function
+// Function to convert bpm to milliseconds for setTimeout() function
 function convertBpmToMilliseconds(bpm) {
     const beatsPerSecond = bpm / 60;
     const msPerBeat = 1000 / beatsPerSecond;
     return msPerBeat;
-  }
+}
 
 // Add an event listener to the tempo range input
 tempoRangeInput.addEventListener('input', function() {
-  currentTempo = tempoRangeInput.value;
-  tempoDisplay.textContent = `${currentTempo} BPM`;
-  millisecondsBetweenClicks = convertBpmToMilliseconds(currentTempo);
+    currentTempo = tempoRangeInput.value;
+    tempoDisplay.textContent = `${currentTempo} BPM`;
+    millisecondsBetweenClicks = convertBpmToMilliseconds(currentTempo);
 
-  //THIS SECTION ALLOWS TEMPO TO BE ADJUSTED DURING PLAYBACK
-  if (togglePlay === true) {
-    clearInterval(intervalID);
-    intervalID = setInterval(() => {
-      clickSound.currentTime = 0;
-      clickSound.play();
-  }, millisecondsBetweenClicks);
-  } else if (togglePlay === false) {
-    return
-  }
+    //THIS SECTION ALLOWS TEMPO TO BE ADJUSTED DURING PLAYBACK
+    if (togglePlay === true) {
+        clearTimeout(timeoutID);
+        playClickSound();
+    }
 });
 
-let intervalID
+function playClickSound() {
+    clickSound.currentTime = 0;
+    clickSound.play();
+    timeoutID = setTimeout(playClickSound, millisecondsBetweenClicks);
+}
 
 start.addEventListener('click', () => {
     togglePlay = true;
-    intervalID = setInterval(() => {
-        clickSound.currentTime = 0;
-        clickSound.play();
-    }, millisecondsBetweenClicks);
+    playClickSound();
 });
 
 stop.addEventListener('click', () => {
     togglePlay = false;
-    clearInterval(intervalID);
+    clearTimeout(timeoutID);
 });
 
 
